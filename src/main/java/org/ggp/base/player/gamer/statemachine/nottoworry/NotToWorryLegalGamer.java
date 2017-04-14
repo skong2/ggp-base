b0360faa60d308a36458977a47d6600cde75cf52
Samuel Kong <samuel.kong18@gmail.com>
2017-04-14 00:20:57 -0700

    whoops

---
+++ src/main/java/org/ggp/base/player/gamer/statemachine/nottoworry/NotToWorryLegalGamer.java
@@ -0,0 +1,93 @@
+package org.ggp.base.player.gamer.statemachine.nottoworry;
+
+import java.util.List;
+
+import org.ggp.base.apps.player.detail.DetailPanel;
+import org.ggp.base.apps.player.detail.SimpleDetailPanel;
+import org.ggp.base.player.gamer.event.GamerSelectedMoveEvent;
+import org.ggp.base.player.gamer.exception.GamePreviewException;
+import org.ggp.base.util.game.Game;
+import org.ggp.base.util.statemachine.Move;
+import org.ggp.base.util.statemachine.StateMachine;
+import org.ggp.base.util.statemachine.cache.CachedStateMachine;
+import org.ggp.base.util.statemachine.exceptions.GoalDefinitionException;
+import org.ggp.base.util.statemachine.exceptions.MoveDefinitionException;
+import org.ggp.base.util.statemachine.exceptions.TransitionDefinitionException;
+import org.ggp.base.util.statemachine.implementation.prover.ProverStateMachine;
+
+public class NotToWorryLegalGamer extends NotToWorryGamer {
+
+	@Override
+	public Move stateMachineSelectMove(long timeout)
+			throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException {
+		// We get the current start time
+		long start = System.currentTimeMillis();
+
+		/**
+		 * We put in memory the list of legal moves from the
+		 * current state. The goal of every stateMachineSelectMove()
+		 * is to return one of these moves. The choice of which
+		 * Move to play is the goal of GGP.
+		 */
+		List<Move> moves = getStateMachine().getLegalMoves(getCurrentState(), getRole());
+
+		// SampleLegalGamer is very simple : it picks the first legal move
+		Move selection = moves.get(0);
+
+		// We get the end time
+		// It is mandatory that stop<timeout
+		long stop = System.currentTimeMillis();
+
+		/**
+		 * These are functions used by other parts of the GGP codebase
+		 * You shouldn't worry about them, just make sure that you have
+		 * moves, selection, stop and start defined in the same way as
+		 * this example, and copy-paste these two lines in your player
+		 */
+		notifyObservers(new GamerSelectedMoveEvent(moves, selection, stop - start));
+		return selection;
+	}
+
+	@Override
+	public void stateMachineMetaGame(long timeout) throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException
+	{
+		// Sample gamers do no metagaming at the beginning of the match.
+	}
+
+	/** This will currently return "SampleGamer"
+	 * If you are working on : public abstract class MyGamer extends SampleGamer
+	 * Then this function would return "MyGamer"
+	 */
+	@Override
+	public String getName() {
+		return getClass().getSimpleName();
+	}
+
+	// This is the default State Machine
+	@Override
+	public StateMachine getInitialStateMachine() {
+		return new CachedStateMachine(new ProverStateMachine());
+	}
+
+	// This is the defaul Sample Panel
+	@Override
+	public DetailPanel getDetailPanel() {
+		return new SimpleDetailPanel();
+	}
+
+	@Override
+	public void stateMachineStop() {
+		// Sample gamers do no special cleanup when the match ends normally.
+	}
+
+	@Override
+	public void stateMachineAbort() {
+		// Sample gamers do no special cleanup when the match ends abruptly.
+	}
+
+	@Override
+	public void preview(Game g, long timeout) throws GamePreviewException {
+		// Sample gamers do no game previewing.
+	}
+
+}

0eba1625ec31cbe8920cd866fbade2105d5170b9
jkamalu <john.kamalu@gmail.com>
2017-04-16 18:26:19 -0700

    Removed some overrides of stop and abort handlers

--- src/main/java/org/ggp/base/player/gamer/statemachine/nottoworry/NotToWorryMinimaxGamer.java
+++ src/main/java/org/ggp/base/player/gamer/statemachine/nottoworry/NotToWorryMinimaxGamer.java
@@ -1,10 +1,14 @@
 package org.ggp.base.player.gamer.statemachine.nottoworry;
 
+import java.util.List;
+import java.util.Random;
+
 import org.ggp.base.apps.player.detail.DetailPanel;
 import org.ggp.base.apps.player.detail.SimpleDetailPanel;
+import org.ggp.base.player.gamer.event.GamerSelectedMoveEvent;
 import org.ggp.base.player.gamer.exception.GamePreviewException;
-import org.ggp.base.player.gamer.statemachine.sample.SampleGamer;
 import org.ggp.base.util.game.Game;
+import org.ggp.base.util.statemachine.Move;
 import org.ggp.base.util.statemachine.StateMachine;
 import org.ggp.base.util.statemachine.cache.CachedStateMachine;
 import org.ggp.base.util.statemachine.exceptions.GoalDefinitionException;
@@ -12,7 +16,37 @@ import org.ggp.base.util.statemachine.exceptions.MoveDefinitionException;
 import org.ggp.base.util.statemachine.exceptions.TransitionDefinitionException;
 import org.ggp.base.util.statemachine.implementation.prover.ProverStateMachine;
 
-public abstract class NotToWorryGamer extends SampleGamer {
+public class NotToWorryMinimaxGamer extends NotToWorryGamer {
+
+	@Override
+	public Move stateMachineSelectMove(long timeout)
+			throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException {
+		// We get the current start time
+		long start = System.currentTimeMillis();
+
+		/**
+		 * We put in memory the list of legal moves from the
+		 * current state. The goal of every stateMachineSelectMove()
+		 * is to return one of these moves. The choice of which
+		 * Move to play is the goal of GGP.
+		 */
+		List<Move> moves = getStateMachine().getLegalMoves(getCurrentState(), getRole());
+
+		Move selection = (moves.get(new Random().nextInt(moves.size())));
+
+		// We get the end time
+		// It is mandatory that stop<timeout
+		long stop = System.currentTimeMillis();
+
+		/**
+		 * These are functions used by other parts of the GGP codebase
+		 * You shouldn't worry about them, just make sure that you have
+		 * moves, selection, stop and start defined in the same way as
+		 * this example, and copy-paste these two lines in your player
+		 */
+		notifyObservers(new GamerSelectedMoveEvent(moves, selection, stop - start));
+		return selection;
+	}
 
 	@Override
 	public void stateMachineMetaGame(long timeout) throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException
